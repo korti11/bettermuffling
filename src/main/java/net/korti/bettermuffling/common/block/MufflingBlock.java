3853764dde5daf5c2e3863b0d740c3c792d8c6e3
Korti <lucas.korthauer@gmail.com>
2019-08-08 16:24:11 +0200

    Added muffling block.

---
+++ src/main/java/net/korti/bettermuffling/common/block/MufflingBlock.java
@@ -0,0 +1,25 @@
+package net.korti.bettermuffling.common.block;
+
+import net.korti.bettermuffling.BetterMuffling;
+import net.minecraft.block.ContainerBlock;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.IBlockReader;
+
+import javax.annotation.Nullable;
+
+public class MufflingBlock extends ContainerBlock {
+
+    public MufflingBlock() {
+        super(Properties.create(Material.WOOL).sound(SoundType.CLOTH));
+        this.setRegistryName(BetterMuffling.MOD_ID, "muffling_block");
+    }
+
+    @Nullable
+    @Override
+    public TileEntity createNewTileEntity(IBlockReader worldIn) {
+        return null;
+    }
+
+}
